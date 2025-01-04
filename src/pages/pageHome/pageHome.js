daba064753686a551d186bf7ecb00afcb231d16c
Vladimír Hanhur <aqgz@email.cz>
2025-01-04 12:16:01 +0100

    update

--- src/pages/pageHome/pageHome.js
+++ src/pages/pageHome/pageHome.js
@@ -2,12 +2,14 @@ import { getHomeMenu } from "../../components/homeMenu/homeMenu";
 import { getWelcomeHome } from "../../components/welcomeHome/welcomeHome";
 import { getServices } from "../../components/services/services";
 import { getSpecialties } from "../../components/specialties/specialties";
+import { getBookAppointment } from "../../components/bookAppointment/bookAppointment";
 import "./pageHome.css";
 
 const homeMenu = getHomeMenu();
 const welcomeHome = getWelcomeHome();
 const services = getServices();
 const specialties = getSpecialties();
+const book = getBookAppointment();
 
 export function getPageHome()
 {
@@ -19,6 +21,7 @@ export function getPageHome()
         welcomeHome,
         services,
         specialties,
+        book,
     );
 
     return wrapper;
