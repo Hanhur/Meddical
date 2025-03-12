51bdcf9cf56934e296baa156ee4cd5fb38cec0e1
Vladimír Hanhur <aqgz@email.cz>
2025-03-12 23:40:13 +0100

    Updates

--- normalize.d1cf9e2f.js
+++ normalize.d1cf9e2f.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39533" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37295" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
