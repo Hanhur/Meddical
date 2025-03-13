f80cb173af1b10a8f423411169a4bf02e03dc575
Vladimír Hanhur <aqgz@email.cz>
2025-03-13 20:26:26 +0100

    Updates

--- pageHome.46aef0d8.js
+++ pageHome.46aef0d8.js
@@ -460,7 +460,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35689" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36325" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
